c2da6e32831ffd20279511da962b5eb7e26c7b91
ShimYS <sim5522@naver.com>
2020-10-04 01:43:51 +0900

    201004 JPA 학습

--- hello-spring/src/main/java/hello/hellospring/service/SpringConpig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConpig.java
@@ -1,23 +1,31 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConpig {
 
-    private DataSource dataSource;
+    /* 순수Jdbc, JdbcTemplate를 사용할 때 주입해준다. */
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConpig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    /* JPA를 사용할 때 주입해준다. */
+
+    private EntityManager em;
 
     @Autowired
-    public SpringConpig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConpig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -27,7 +35,8 @@ public class SpringConpig {
 
     @Bean
     public MemberRepository memberRepository () {
-        return new JdbcTemplateMemberRepository(dataSource);  // JdbcTemplate를 활용한 데이터 저장
+        return new JpaMemberRepository(em);
+//      return new JdbcTemplateMemberRepository(dataSource);  // JdbcTemplate를 활용한 데이터 저장
 //      return new JdbcMemberRepository(dataSource);          // 순수Jdbc를 활용한 데이터 저장
 //      return new MemoryMemberRepository();                  // 메모리에 데이터 저장
     }
