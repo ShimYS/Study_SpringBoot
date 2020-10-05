60c078c24880fb270a44d856a8b5d333e01189dd
ShimYS <sim5522@naver.com>
2020-10-05 17:34:14 +0900

    201005 스프링 데이터 JPA 학습

--- hello-spring/src/main/java/hello/hellospring/service/SpringConpig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConpig.java
@@ -21,23 +21,32 @@ public class SpringConpig {
 
     /* JPA를 사용할 때 주입해준다. */
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConpig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    /* 스프링 데이터 JPA를 사용할 때 주입해준다. */
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConpig(EntityManager em) {
-        this.em = em;
+    public SpringConpig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository () {
-        return new JpaMemberRepository(em);
-//      return new JdbcTemplateMemberRepository(dataSource);  // JdbcTemplate를 활용한 데이터 저장
-//      return new JdbcMemberRepository(dataSource);          // 순수Jdbc를 활용한 데이터 저장
-//      return new MemoryMemberRepository();                  // 메모리에 데이터 저장
-    }
+    /* 스프링 데이터 JPA를 사용할 때는 사용하지 않는다. */
+//    @Bean
+//    public MemberRepository memberRepository () {
+////      return new JpaMemberRepository(em);                   // JPA를 활용한 데이터 저장
+////      return new JdbcTemplateMemberRepository(dataSource);  // JdbcTemplate를 활용한 데이터 저장
+////      return new JdbcMemberRepository(dataSource);          // 순수Jdbc를 활용한 데이터 저장
+////      return new MemoryMemberRepository();                  // 메모리에 데이터 저장
+//    }
 }
