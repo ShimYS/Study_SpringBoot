cd5f16c0ad5735fb5d48ef33eef3b2dc22e4bf51
ShimYS <sim5522@naver.com>
2020-10-08 19:15:45 +0900

    201008 jpashop 도메인 분석 설계

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // JPA 구현 라이브러리가 객체를 생성할 때 리플렉션 같은 기술을 사용할 수 있도록 지원하기 위함.
+    // JPA 스펙상 Entity, Embedable 타입은 자바 기본 생성자를 public 또는 protected로 설정해야함. (protected가 그나마 더 안전)
+    protected Address() { }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
